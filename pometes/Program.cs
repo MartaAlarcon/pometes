7e48920f060c965b63f3390a8fdffb64491600be
MartaAlarcon <marta.alarcon.7e7@itb.cat>
2024-01-10 11:56:12 +0100

    Add project files.

---
+++ pometes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+namespace Pometes
+{
+    public class EjPometes
+    {
+        public static void Main()
+        {
+            const string MsgPometes = "Quantes pometes té el pomer?: ";
+            int numPometes;
+            Console.WriteLine(MsgPometes);
+            numPometes  =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(caigudaPometes(numPometes));
+            
+
+        }
+        public static int caigudaPometes (int numPometes)
+        {
+            if (numPometes == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                Console.WriteLine($"{numPometes} pometes té el pomer\r\nde {numPometes} una de {numPometes} una\r\n{numPometes} pometes té el pomer\r\nde {numPometes} una se'n caigué");
+                Console.WriteLine();
+                return caigudaPometes(numPometes - 1);
+            }
+        }
+    }
+}
